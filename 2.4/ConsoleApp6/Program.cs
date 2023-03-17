c3177e31fd6d2772ff9fbe38f1fbbcfa932a6399
Mikhnyuk Denis Alexevich <83284438+eqweqrt@users.noreply.github.com>
2023-03-17 17:49:26 +0700

    Add files via upload

---
+++ 2.4/ConsoleApp6/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string rim = " MDIX ";
+
+            int sum = 0;
+
+                for (int i = 1; i < rim.Length - 1; i++)
+                {
+                    switch (rim[i])
+                    {
+                        case 'I' :
+                        {
+                            if (rim[i+1] == 'V')
+                            {
+                                sum += 4;
+                            }
+                            
+                            else if (rim[i+1] == 'X')
+                            {
+                                sum += 9;
+                            }
+                            else
+                            {
+                                sum += 1;
+                            }
+                            break;
+                        }
+                        case 'V' :
+                        {
+                            sum += 5;
+                            break;
+                        }
+                        case 'X' :
+                        {
+                            if(rim[i+1] == 'L')
+                            {
+                                sum = sum + 40;
+                            }
+                            else if (rim[i+1] == 'C')
+                            {
+                                sum = sum + 90;
+                            }
+                            else
+                            {
+                                sum += 10;
+                            }
+                            break;
+                        }
+                        case 'L' :
+                        {
+                            sum += 50;
+                            break;
+                        }
+                        case 'C' :
+                        {
+                            if(rim[i+1] == 'D')
+                            { 
+                                sum += 400;
+                            }
+                            else if (rim[i+1] == 'M')
+                            {
+                                sum += 900;
+                            }
+                            else
+                            {
+                                sum += 100;
+                            }
+
+                            sum +=300;
+                            break;
+                        }
+                        case 'D' :
+                        {
+                            sum += 500;
+                            break;
+                        }
+                        case 'M' :
+                        {
+                            sum += 1000;
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine(sum);
+        }
+    }
+}
+
